5a4a881dcbceaf3db953a53acc623bf4957e2ae7
ngeor26 <ngeor26@germantownacademy.org>
2024-08-07 15:58:49 -0400

    Before DLL Import

---
+++ Assets/Raygun/editor/evaporate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class evaporate : MonoBehaviour
+{
+   public void OnCollisionEnter(Collider other)
+   {
+    
+   }
+}
--- Assets/Raygun/editor/shoot.cs
+++ Assets/Raygun/editor/shoot.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations.Rigging;
-using ThunderRoad;
 
 public class shoot : MonoBehaviour
 {
